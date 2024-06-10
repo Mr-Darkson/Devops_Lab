4af352cbccdd9a8ac96c0551b232d873f5ec4198
EchoTerror <najikore@mail.ru>
2024-06-10 13:29:15 +0300

    "Initial commit"

---
+++ src/main/java/com/example/devopstask/controllers/HelloController.java
@@ -0,0 +1,44 @@
+package com.example.devopstask.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, boy!";
+    }
+
+    @GetMapping("/help")
+    public String getHelp() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Добро пожаловать!\n");
+        builder.append("Возможные команды:\n");
+        builder.append("/append a='', b=''\n");
+        builder.append("/substract a='', b=''\n");
+        builder.append("/divide a='', b=''\n");
+        builder.append("/multiply a='', b=''\n");
+        return builder.toString();
+    }
+
+    @GetMapping("/append")
+    public double append(@RequestParam("a") double a, @RequestParam("b") double b) {
+        return a + b;
+    }
+    @GetMapping("/substract")
+    public double substract(@RequestParam("a") double a, @RequestParam("b") double b) {
+        return a - b;
+    }
+
+    @GetMapping("/divide")
+    public double divide(@RequestParam("a") double a, @RequestParam("b") double b) {
+        return a / b;
+    }
+    @GetMapping("/multiply")
+    public double multiply(@RequestParam("a") double a, @RequestParam("b") double b) {
+        return a * b;
+    }
+}
---
+++ src/test/java/com/example/devopstask/DevopsTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopstask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
