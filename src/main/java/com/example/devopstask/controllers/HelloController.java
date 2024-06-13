bd16603e59bf34cd46f71b587f596e2dcbf2c78c
EchoTerror <najikore@mail.ru>
2024-06-13 08:17:30 +0300

    Change getmapping

--- src/main/java/com/example/devopstask/controllers/HelloController.java
+++ src/main/java/com/example/devopstask/controllers/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/helloBro")
     public String sayHello() {
         return "Hello, boy!";
     }
